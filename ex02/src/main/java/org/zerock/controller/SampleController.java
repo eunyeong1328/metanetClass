4c15343b12f0d87354ebdb5f81ca883fd4610bf5
eunyoung1328 <eunyeong1328@daum.net>
2023-03-08 10:26:40 +0900

    feat: 단순 로그인 처리security-context.xml

---
+++ ex02/src/main/java/org/zerock/controller/SampleController.java
@@ -0,0 +1,30 @@
+package org.zerock.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@RequestMapping("/sample")
+@Log4j
+public class SampleController {
+	
+	@GetMapping("/all")
+	public void doAll() {
+		
+	}
+	
+	@GetMapping("/member")
+	public void doMember() {
+		
+	}
+
+	@GetMapping("/admin")
+	public void doAdamin() {
+		
+	}
+
+
+}
