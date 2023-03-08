cd2d40a36bddc64ff17eb34adfbea656c8112c8d
eunyoung1328 <eunyeong1328@daum.net>
2023-03-08 10:50:18 +0900

    feat: 접근 제한 메세지 처리

--- ex02/src/main/java/org/zerock/controller/SampleController.java
+++ ex02/src/main/java/org/zerock/controller/SampleController.java
@@ -1,6 +1,8 @@
 package org.zerock.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -25,6 +27,12 @@ public class SampleController {
 	public void doAdamin() {
 		
 	}
+	
+	@GetMapping("/accessError")
+	public void accessDenied(Authentication auth, Model model) {
+		model.addAttribute("msg","Access Denied");
+	}
+
 
 
 }
